2be687cee1a889b10e97bd36a5a00efd101f3529
manishhh-27 <manishrahul198@gmail.com>
2024-06-30 19:08:58 +0530

    first commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require('connect-mongo');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl =process.env.DB_URL;
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     
@@ -37,6 +37,7 @@ mongoose.connect(dbUrl, {
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
